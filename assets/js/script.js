8e9ff80b7737334ad56d0dd4f37ca9a6648588f5
tetaPehta <infoshadowriver@gmail.com>
2023-02-19 16:58:33 +0000

    Add basic java function

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnwser() {
+
+}
+
+function calculateCorrectAnwser() {
+
+}
+
+function incrementScore() {
+
+}
+
+function displayWrongAnwser() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
