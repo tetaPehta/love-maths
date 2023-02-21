15db2956084ff8aa17a3663d4a506bd23945769b
tetaPehta <infoshadowriver@gmail.com>
2023-02-21 05:39:27 +0000

    Add multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,8 @@ function runGame(gameType) {
 
     if (gameType === "addition"){
         displayAdditionQuestion(num1, num2);
+    } else if (gameType ==="multiply") {
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unown game type: ${gameType}. Aborting!`;
@@ -66,6 +68,8 @@ function calculateCorrectAnwser() {
 
        if (operator === "+") {
            return [operand1 + operand2, "addition"];
+       } else if (operator === "x") {
+          return [operand1* operand2, "multiply"];
        } else {
            alert(`Unimplemented operator ${operator}`);
            throw `Unimplemented operator ${operator}. Aborting!`;
@@ -92,6 +96,7 @@ function displayWrongAnwser() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
@@ -101,6 +106,10 @@ function displaySubtractQuestion() {
 
 }
 
-function displayMultiplyQuestion() {
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
